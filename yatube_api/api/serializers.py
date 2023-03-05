d7351e2fe428c148da2ab2daaaab40ec4b00bc90
Юлия Мелик-Гусейнова <yu.melik-guseinova@yandex.ru>
2023-03-05 11:03:26 +0300

    исправлено

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 from rest_framework.validators import UniqueTogetherValidator
 
-
 from posts.models import Comment, Follow, Group, Post, User
 
 
@@ -39,7 +38,7 @@ class CommentSerializer(serializers.ModelSerializer):
 class FollowSerializer(serializers.ModelSerializer):
     user = serializers.SlugRelatedField(
         slug_field='username',
-        queryset=User.objects.all(),
+        read_only=True,
         default=serializers.CurrentUserDefault())
     following = serializers.SlugRelatedField(
         slug_field='username',
@@ -57,7 +56,8 @@ class FollowSerializer(serializers.ModelSerializer):
         )
 
     def validate(self, data):
-        if data['user'] == data['following']:
+        if self.context['request'].user == data['following']:
             raise serializers.ValidationError(
-                'Нельзя подписаться на самого себя!')
+                'Нельзя подписаться на самого себя!'
+            )
         return data
--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -5,19 +5,19 @@ from rest_framework.routers import DefaultRouter
 from api.views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
 
 
-v1 = DefaultRouter()
+v1_router = DefaultRouter()
 
-v1.register(r'v1/posts', PostViewSet, basename='post')
-v1.register(r'v1/groups', GroupViewSet, basename='group')
-v1.register(r'v1/follow', FollowViewSet, basename='follow')
-v1.register(
+v1_router.register(r'v1/posts', PostViewSet, basename='post')
+v1_router.register(r'v1/groups', GroupViewSet, basename='group')
+v1_router.register(r'v1/follow', FollowViewSet, basename='follow')
+v1_router.register(
     r'v1/posts/(?P<post_id>\d+)/comments',
     CommentViewSet, basename='comments',
 )
 
 urlpatterns = [
     path('v1/api-token-auth/', views.obtain_auth_token),
-    path('', include(v1.urls)),
+    path('', include(v1_router.urls)),
     path('v1/', include('djoser.urls')),
     path('v1/', include('djoser.urls.jwt')),
 ]
